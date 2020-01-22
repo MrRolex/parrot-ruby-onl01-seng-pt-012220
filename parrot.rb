a327c85c0521cee1e59d7ac35f3e3dbd018b49ad
Edward Williams <eddie_w38@yahoo.com>
2020-01-22 15:22:08 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(name)
-  puts 
+def parrot (phrase = "Squawk!")
+  call = phrase
+  puts (phrase)
+  call
 end
   
